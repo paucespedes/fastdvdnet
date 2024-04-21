69cd29e4872114b58a43fc7611f544b2c8963fdb
paucespedes <paucespedes.code@gmail.com>
2024-04-22 00:06:37 +0200

    reset torch

--- train_fastdvdnet.py
+++ train_fastdvdnet.py
@@ -12,6 +12,7 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import time
 import argparse
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from models import FastDVDnet
